e3fdfeebb826b61c718ee0b2539d419f6eba08d5
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-02-14 18:19:32 -0200

    Update product image

--- react/ProductImage.js
+++ react/ProductImage.js
@@ -1,18 +1,40 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Fragment, useState } from "react";
+import PropTypes from "prop-types";
+import ReactResizeDetector from "react-resize-detector";
 
-import { utils } from 'vtex.my-account-commons'
+import { utils } from "vtex.my-account-commons";
 
-const { fixImageUrl } = utils
+const { fixImageUrl } = utils;
 
 function ProductImage({ className, url, alt }) {
-  return <img className={className} src={fixImageUrl(url)} alt={alt} />
+
+  const [maxWidth, setMaxWidth] = useState(50)
+  const [maxHeight, setMaxHeight] = useState(50)
+  const [lastUrl, setLastUrl] = useState(url)
+
+  const resizeHandler = (width, height) => {
+    if(width > maxWidth) setMaxWidth(width)
+    if(height > maxHeight) setMaxHeight(height)
+
+    return
+  }
+
+  return (
+    <Fragment>
+      <ReactResizeDetector handleWidth handleHeight onResize={(width, height) => resizeHandler(width, height)} />
+      <img
+        className={`${className}`}
+        src={fixImageUrl(lastUrl, maxWidth, maxHeight)}
+        alt={alt}
+      />
+    </Fragment>
+  )
 }
 
 ProductImage.propTypes = {
   className: PropTypes.string,
   url: PropTypes.string,
-  alt: PropTypes.string,
-}
+  alt: PropTypes.string
+};
 
-export default ProductImage
+export default ProductImage;
