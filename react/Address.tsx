10a6e13d8941903c4692cf8713c3fe0dd3b51b31
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-02-14 23:02:10 -0200

    Add Addres, ButtonLink and CustomerInfo components from OrderPlaced

---
+++ react/Address.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent } from 'react'
+import { AddressRules, AddressSummary } from 'vtex.address-form'
+
+interface Props {
+  address: Address
+  pickup?: Parcel
+}
+
+const Address: FunctionComponent<Props> = ({ address, pickup }) => {
+  return (
+    <div className="c-muted-1 mb5" data-testid="address-component">
+      {pickup && <p className="c-on-base">{pickup.pickupFriendlyName}</p>}
+      <AddressRules country={address.country} shouldUseIOFetching>
+        <AddressSummary address={address} />
+      </AddressRules>
+    </div>
+  )
+}
+
+export default Address
